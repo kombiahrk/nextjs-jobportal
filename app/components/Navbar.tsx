f44bc494cffcb55249bc4a7929d2b3655bd8ae69
Kombiah <kombiahrk@yahoo.com>
2024-07-15 18:47:12 +0530

    Update Navbar.tsx to add the LogoImage for the site

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import LogoImage from '@/public/images/logo.png'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='h-[13vh] overflow-hidden shadow-md'>
+      <div className='w-[90%] md:w-[80%] h-[100%] mx-auto flex items-center justify-between'>
+        {/* logo */}
+        <div className='w-[150px] h-[150px] md:w-[250px] md:h-[250px]'>
+          <Link href="/">
+            <Image
+              src={LogoImage}
+              alt="logo"
+              width={250}
+              height={250}
+              className='w-[100%] h-[100%]'
+            />
+          </Link>
+        </div>
+        {/* menu */}
+      </div>
+    </div>
   )
 }
 
